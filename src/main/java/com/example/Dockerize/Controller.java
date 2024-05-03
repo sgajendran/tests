84a6f00fd70f6c25d6ffeca5e9e799ecfd5f5baf
be4929 <sgn1676@gmail.com>
2024-05-03 19:41:48 +0530

    first commit

---
+++ src/main/java/com/example/Dockerize/Controller.java
@@ -0,0 +1,17 @@
+package com.example.Dockerize;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+	
+	@GetMapping(path = "/hello")
+	
+	public String sayHello() {
+		
+		return "Hi All";
+		
+	}
+
+}
